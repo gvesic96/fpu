c60bd7a17b486d77de9c56d0aa7e93620f3e5633
gvesic96 <gilevesic@gmail.com>
2025-06-23 16:21:10 +0200

    Small alu overflow for 2's complement bug fixed

--- design/small_alu.vhd
+++ design/small_alu.vhd
@@ -41,9 +41,15 @@ entity small_alu is
 end small_alu;
 
 architecture Behavioral of small_alu is
-    
+
+    signal result_s : unsigned(WIDTH downto 0) := (others=>'0'); --jedan bit vise u odnosu na ulaz exp je 8 bita a medjurezultat treba 9 zbog komplementa dvojke
+    --zbog komplementa dvojke je neophodan jos jedan dodatni bit jer su ulazi unsigned odnosno biased vrednosti od 0 do 255
 begin
     
-    result <= std_logic_vector(unsigned(op1) + (not(unsigned(op2))+1));
+
+
+    result_s <= (unsigned('0' & op1) + (not(unsigned('0' & op2))+1));
+    result <= std_logic_vector(result_s(WIDTH-1 downto 0));
+
 
 end Behavioral;
