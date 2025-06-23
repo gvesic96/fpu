b98e827bf5f0e21977542037f6e93fed50d96af3
gvesic96 <gilevesic@gmail.com>
2025-06-23 17:37:50 +0200

    Output of small ALU set to 9 bits for result sign comparison

--- design/small_alu.vhd
+++ design/small_alu.vhd
@@ -36,20 +36,20 @@ entity small_alu is
     Generic ( WIDTH : positive := 32);
     Port ( op1 : in STD_LOGIC_VECTOR(WIDTH-1 downto 0);
            op2 : in STD_LOGIC_VECTOR(WIDTH-1 downto 0);
-           result : out STD_LOGIC_VECTOR(WIDTH-1 downto 0)
+           result : out STD_LOGIC_VECTOR(WIDTH downto 0)--9 bita za izlas SMALL ALUa zbog znaka po kome odredjuje da li je broj pozitivan ili negativan
            );
 end small_alu;
 
 architecture Behavioral of small_alu is
 
-    signal result_s : unsigned(WIDTH downto 0) := (others=>'0'); --jedan bit vise u odnosu na ulaz exp je 8 bita a medjurezultat treba 9 zbog komplementa dvojke
+    --signal result_s : unsigned(WIDTH downto 0) := (others=>'0'); --jedan bit vise u odnosu na ulaz exp je 8 bita a medjurezultat treba 9 zbog komplementa dvojke
     --zbog komplementa dvojke je neophodan jos jedan dodatni bit jer su ulazi unsigned odnosno biased vrednosti od 0 do 255
 begin
     
 
-
-    result_s <= (unsigned('0' & op1) + (not(unsigned('0' & op2))+1));
-    result <= std_logic_vector(result_s(WIDTH-1 downto 0));
+    --small alu mora imati izlaz od 9 bita ako je WIDTH 8, result ce biti 8 downto 0 sto je 9 bita
+    result <= std_logic_vector(unsigned('0' & op1) + (not(unsigned('0' & op2))+1));
+    --result <= std_logic_vector(result_s(WIDTH-1 downto 0));
 
 
 end Behavioral;
